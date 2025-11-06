774bd323dfb2e2d212bd9837193ec01c2c39a50d
Matthew <149590049+MatthewW2027@users.noreply.github.com>
2025-11-06 19:22:42 +0000

    2.4 notes

--- Main.java
+++ Main.java
@@ -57,5 +57,24 @@ public class Main {
          System.out.println("Flipped tails! Wear black.");
       }
 
+      // MULTI-WAY SELECTION 
+      // 1. NESTING if statments is like asking "follow-up questions"
+      int age = 16; 
+      boolean havePermit = true; 
+      System.out.println("Let's see if you can take this road test...");
+      if (age >= 16) {
+         System.out.println("Alright, I guess you're old enough...");
+         // After passing the first condition (age >= 16) you ask a FOLLOWUP (another if statement)
+
+         if (havePermit == true) {
+            System.out.println("Fine, get in the car."); 
+         }
+      } 
+      else {
+         System.out.println("You're too young. "); 
+      }
+
+
+
    }
 }
